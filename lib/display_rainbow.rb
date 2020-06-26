0212e11e4214eddfd3b630492d4ba2c73fd57500
Kevin Botero <kbotero201@gmail.com>
2020-06-26 16:03:37 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,8 @@
 def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
-  puts "Contact: #{person[3]}"
 end
 
-bob     = ["Bob", 46, "Juggler", "555-555-5555"]
+bob     = ["Bob", 46, "Juggler"]
 stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
 
 print_business_card(bob)
