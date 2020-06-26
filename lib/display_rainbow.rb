5388f7c6206fdc6747a111916d04ec5a03dbbec9
Kevin Botero <kbotero201@gmail.com>
2020-06-26 16:01:19 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,31 +1,8 @@
-# Write your #display_rainbow method here
-#def display_rainbow(colors)
-#  puts "R: #{colors(0)}, O: #{colors(1)}, Y: #{colors(2)}, G: #{colors(3)}, B: #{colors(4)}, I: #{colors(5)}, V: #{colors(6)}"
-#end
 
 
-#display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+bob     = ["Bob", 46, "Juggler", "555-555-5555"]
+stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
 
-#colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-#puts "R: #{colors(0)}, O: #{colors(1)}, Y: #{colors(2)}, G: #{colors(3)}, B: #{colors(4)}, I: #{colors(5)}, V: #{colors(6)}"
-
-#bob     = ["Bob", 46, "Juggler"]
-#stefani = ["Stefani", 49, "Firefighter"]
-
-#name = bob[0]
- 
-#puts "#{bob[0]}"
-
-#colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-
-
-
-def display_rainbow(colors)
-  puts "R: #{colors(0)}, O: #{colors(1)}, Y: #{colors(2)}, G: #{colors(3)}, B: #{colors(4)}, I: #{colors(5)}, V: #{colors(6)}"
-end 
-
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-
-display_rainbow(colors)
+print_business_card(bob)
 
 
