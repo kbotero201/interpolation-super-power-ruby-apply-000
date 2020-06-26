9567358a8d07de3441d6166f46d572a75ac529d8
Kevin Botero <kbotero201@gmail.com>
2020-06-26 16:04:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,9 @@
-def print_business_card(person)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+def print_business_card(rainbow)
+  puts "Name: #{rainbow[0]}, Age: #{rainbow[1]}, Occupation: #{rainbow[2]}"
 end
 
-colors     = ["Bob", 46, "Juggler"]
-stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-print_business_card(bob)
+print_business_card(colors)
 
 
