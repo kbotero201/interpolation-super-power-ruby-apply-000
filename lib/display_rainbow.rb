08541674c2ed840047a0b4671be368da808c861f
Kevin Botero <kbotero201@gmail.com>
2020-06-26 16:05:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,9 @@
-def print_business_card(rainbow)
+def display_rainbow(rainbow)
   puts "Name: #{rainbow[0]}, Age: #{rainbow[1]}, Occupation: #{rainbow[2]}"
 end
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-print_business_card(colors)
+display_rainbow(colors)
 
 
